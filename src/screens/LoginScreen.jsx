f74ebb9d7c22de0643523afea64f0a9775ebddcc
Samuel Conde <samuelconra@Samuels-MacBook-Air.local>
2025-10-21 16:56:03 -0600

    Dockerfile configured

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -12,7 +12,7 @@ function LoginScreen () {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:3000/auth/login", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
